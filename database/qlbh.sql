e62fb18828381c22589c05939bbda2c49a50c6d0
Trung Anh <tieupink312@gmail.com>
2023-10-18 15:04:41 +0700

    db

--- database/qlbh.sql
+++ database/qlbh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 16, 2023 lúc 08:53 AM
+-- Thời gian đã tạo: Th10 18, 2023 lúc 10:03 AM
 -- Phiên bản máy phục vụ: 10.4.25-MariaDB
 -- Phiên bản PHP: 8.1.10
 
@@ -24,27 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `admin`
---
-
-CREATE TABLE `admin` (
-  `admin_id` int(11) NOT NULL,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `full_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `admin`
---
-
-INSERT INTO `admin` (`admin_id`, `username`, `password`, `email`, `full_name`) VALUES
-(1, 'admin1', 'password1', 'admin1@example.com', 'Admin 1');
-
--- --------------------------------------------------------
-
---
 -- Cấu trúc bảng cho bảng `cart`
 --
 
@@ -54,25 +33,6 @@ CREATE TABLE `cart` (
   `product_id` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `cart`
---
-
-INSERT INTO `cart` (`cart_id`, `customer_id`, `product_id`, `quantity`) VALUES
-(1, 1, 1, 5),
-(2, 1, 3, 1),
-(3, 2, 2, 1),
-(4, 3, 4, 1),
-(5, 4, 5, 2),
-(7, 1, 1, 0),
-(8, 1, 1, 0),
-(9, 1, 2, 0),
-(10, 1, 2, 2),
-(11, 1, 2, 2),
-(12, 1, 2, 2),
-(13, 1, 2, 2),
-(14, 1, 5, 2);
 
 -- --------------------------------------------------------
 
@@ -108,19 +68,21 @@ CREATE TABLE `customers` (
   `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `phone` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `role` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `customers`
 --
 
-INSERT INTO `customers` (`customer_id`, `first_name`, `last_name`, `email`, `phone`, `password`) VALUES
-(1, 'John', 'Doe', 'johndoe@example.com', '1234567890', 'anhtrung'),
-(2, 'Jane', 'Smith', 'janesmith@example.com', '9876543210', 'anhtrung'),
-(3, 'Michael', 'Johnson', 'michaeljohnson@example.com', '5555555555', 'anhtrung'),
-(4, 'Emily', 'Davis', 'emilydavis@example.com', '1111111111', 'anhtrung'),
-(5, 'Samuel', 'Wilson', 'samuelwilson@example.com', '9999999999', 'anhtrung');
+INSERT INTO `customers` (`customer_id`, `first_name`, `last_name`, `email`, `phone`, `password`, `role`) VALUES
+(2, 'Jane', 'Smith', 'janesmith@example.com', '9876543210', 'anhtrung', 2),
+(3, 'Michael', 'Johnson', 'michaeljohnson@example.com', '5555555555', 'anhtrung', 2),
+(4, 'Emily', 'Davis', 'emilydavis@example.com', '1111111111', 'anhtrung', 2),
+(5, 'Samuel', 'Wilson', 'samuelwilson@example.com', '9999999999', 'anhtrung123', 2),
+(7, 'Admin', '1234', 'admin@gmail.com', '0353106446', 'admin', 1),
+(9, 'John1', 'ddddd', '1', '0353106446', '1', 2);
 
 -- --------------------------------------------------------
 
@@ -145,6 +107,22 @@ INSERT INTO `manufacturers` (`manufacturer_id`, `manufacturer_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `payments`
+--
+
+CREATE TABLE `payments` (
+  `payment_id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `cart_id` int(11) DEFAULT NULL,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `status` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `quantity` int(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `products`
 --
 
@@ -162,11 +140,12 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `description`, `price`, `manufacturer_id`, `image_url`) VALUES
-(1, 'Xoong nồi', 'Description of Product 1', '10.99', 1, 'https://suno.vn/blog/wp-content/uploads/2020/06/boxme-kinh-doanh-hang-gia-dung-1250x800.jpg'),
+(1, 'Xoong nồi1', 'Description of Product 1', '10.99', 1, 'https://suno.vn/blog/wp-content/uploads/2020/06/boxme-kinh-doanh-hang-gia-dung-1250x800.jpg'),
 (2, 'Product 2', 'Description of Product 2', '19.99', 2, 'https://inoxfivestar.com/Uploads/Image/bo-noi-Cookever/bo-noi-cao-cap-cookever-DD.jpg'),
 (3, 'Product 3', 'Description of Product 3', '5.99', 1, 'https://gotrangtri.vn/wp-content/uploads/2018/04/phong-thuy-nha-o-1.png'),
 (4, 'Product 4', 'Description of Product 4', '15.99', 3, 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Th%E1%BB%9Bt.jpg/300px-Th%E1%BB%9Bt.jpg'),
-(5, 'Product 5', 'Description of Product 5', '8.99', 2, 'https://quatdien.com.vn/wp-content/uploads/2020/07/b400.jpg');
+(5, 'Product 5', 'Description of Product 5Description of Product 5Description of Product 5Description of Product 5Description of Product 5Description of Product 5Description of Product 5Description of Product 5', '8.99', 2, 'https://quatdien.com.vn/wp-content/uploads/2020/07/b400.jpg'),
+(9, 'product 3', '1', '1.00', 2, '1');
 
 -- --------------------------------------------------------
 
@@ -189,40 +168,9 @@ INSERT INTO `product_categories` (`product_id`, `category_id`) VALUES
 (3, 1),
 (4, 4);
 
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `reviews`
---
-
-CREATE TABLE `reviews` (
-  `review_id` int(11) NOT NULL,
-  `product_id` int(11) DEFAULT NULL,
-  `customer_id` int(11) DEFAULT NULL,
-  `rating` int(11) DEFAULT NULL,
-  `comment` text COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `reviews`
---
-
-INSERT INTO `reviews` (`review_id`, `product_id`, `customer_id`, `rating`, `comment`) VALUES
-(1, 1, 1, 4, 'Great product! Highly recommended.'),
-(2, 2, 3, 5, 'Excellent quality and fast delivery.'),
-(3, 3, 2, 3, 'Average product, could be better.'),
-(4, 4, 1, 4, 'Interesting book, couldn\'t put it down.'),
-(5, 2, 4, 2, 'Poor quality, not worth the price.');
-
 --
 -- Chỉ mục cho các bảng đã đổ
 --
-
---
--- Chỉ mục cho bảng `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`admin_id`);
 
 --
 -- Chỉ mục cho bảng `cart`
@@ -252,6 +200,15 @@ ALTER TABLE `manufacturers`
   ADD PRIMARY KEY (`manufacturer_id`);
 
 --
+-- Chỉ mục cho bảng `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `cart_id` (`cart_id`);
+
+--
 -- Chỉ mục cho bảng `products`
 --
 ALTER TABLE `products`
@@ -266,28 +223,14 @@ ALTER TABLE `product_categories`
   ADD KEY `category_id` (`category_id`);
 
 --
--- Chỉ mục cho bảng `reviews`
---
-ALTER TABLE `reviews`
-  ADD PRIMARY KEY (`review_id`),
-  ADD KEY `product_id` (`product_id`),
-  ADD KEY `customer_id` (`customer_id`);
-
---
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
-
---
--- AUTO_INCREMENT cho bảng `admin`
---
-ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT cho bảng `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT cho bảng `categories`
@@ -299,7 +242,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT cho bảng `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT cho bảng `manufacturers`
@@ -308,16 +251,16 @@ ALTER TABLE `manufacturers`
   MODIFY `manufacturer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT cho bảng `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
 -- AUTO_INCREMENT cho bảng `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT cho bảng `reviews`
---
-ALTER TABLE `reviews`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
@@ -331,6 +274,14 @@ ALTER TABLE `cart`
   ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
 
 --
+-- Các ràng buộc cho bảng `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+  ADD CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
+  ADD CONSTRAINT `payments_ibfk_3` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`);
+
+--
 -- Các ràng buộc cho bảng `products`
 --
 ALTER TABLE `products`
@@ -342,13 +293,6 @@ ALTER TABLE `products`
 ALTER TABLE `product_categories`
   ADD CONSTRAINT `product_categories_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
   ADD CONSTRAINT `product_categories_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
-
---
--- Các ràng buộc cho bảng `reviews`
---
-ALTER TABLE `reviews`
-  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
-  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
